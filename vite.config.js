b917b9ab9c3d66ff0c0d5e1f96d0b116290cd34b
Kevin Dubbeld <kevin.dubbeld@gmail.com>
2025-02-22 21:09:59 +0100

    npm update broken fixed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
-        react(),
     ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
+        },
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
 });
