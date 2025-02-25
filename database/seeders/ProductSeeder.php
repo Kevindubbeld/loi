e7fd3cae6ef1440d8ac12c354fa684570460cb49
Kevin Dubbeld <kevin.dubbeld@gmail.com>
2025-02-25 21:07:13 +0100

    IMG_URL links bijgewerkt

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -126,7 +126,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Jonagold Appels',
                 'price' => 2.99,
                 'short_description' => 'Heerlijke Jonagold appels per kg',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_jonagold_appels.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/29092023_1695995626316_1695995631076_8711715881748_1.png',
                 'quantity' => 300,
             ],
             [
@@ -134,7 +134,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Elstar Appels',
                 'price' => 3.49,
                 'short_description' => 'Verse Elstar appels per kg',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_elstar_appels.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/10082023_1691677392872_1691677395732_8711715881755_1.png',
                 'quantity' => 250,
             ],
             [
@@ -142,7 +142,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Bananen',
                 'price' => 1.99,
                 'short_description' => 'Rijpe bananen per kg',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_bananen.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/04022025_1738679565474_1738679569587_8718452709458_1.png',
                 'quantity' => 400,
             ],
             [
@@ -150,7 +150,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Volle Melk',
                 'price' => 0.99,
                 'short_description' => 'Verse volle melk 1L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_volle_melk.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/04062024_1717490780567_1717490789623_8718452645220_596.png',
                 'quantity' => 500,
             ],
             [
@@ -158,7 +158,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Halfvolle Melk',
                 'price' => 0.89,
                 'short_description' => 'Verse halfvolle melk 1L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_halfvolle_melk.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/29032024_1711693986752_1711693995195_8718452753383_596.png',
                 'quantity' => 450,
             ],
             [
@@ -166,7 +166,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Goudse Kaas',
                 'price' => 7.99,
                 'short_description' => 'Goudse kaas per 500g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_goudse_kaas.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/14112023_1699975989714_1699976002168_8718452695102_1.png',
                 'quantity' => 60,
             ],
             [
@@ -174,7 +174,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Edammer Kaas',
                 'price' => 6.99,
                 'short_description' => 'Edammer kaas per 500g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_edammer_kaas.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/10082023_1691674996666_1691675028548_8710912303428_1.png',
                 'quantity' => 70,
             ],
             [
@@ -182,7 +182,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Wit Brood',
                 'price' => 1.49,
                 'short_description' => 'Vers wit brood',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_wit_brood.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/19042024_1713526452274_1713526458149_8718452636488_1.png',
                 'quantity' => 200,
             ],
             [
@@ -190,7 +190,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Bruin Brood',
                 'price' => 1.69,
                 'short_description' => 'Vers bruin brood',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_bruin_brood.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/22042024_1713800058771_1713800062878_8718452652839_1.png',
                 'quantity' => 180,
             ],
             [
@@ -198,7 +198,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Appeltaart',
                 'price' => 4.99,
                 'short_description' => 'Heerlijke appeltaart',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_appeltaart.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/21082024_1724250854682_1724250864172_8718452871728_1.png',
                 'quantity' => 50,
             ],
             [
@@ -206,7 +206,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Slagroomtaart',
                 'price' => 5.99,
                 'short_description' => 'Heerlijke slagroomtaart',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_slagroomtaart.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/18092023_1695042191356_1695042196806_8718452476121_1.png',
                 'quantity' => 40,
             ],
             [
@@ -214,7 +214,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Cola',
                 'price' => 1.49,
                 'short_description' => 'Verfrissende cola 1.5L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_cola.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/12012024_1705031790215_1705031798140_429169_PAK_05000112646856_H1N1.png',
                 'quantity' => 300,
             ],
             [
@@ -222,7 +222,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Sinas',
                 'price' => 1.49,
                 'short_description' => 'Verfrissende sinas 1.5L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_sinas.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/17072024_1721205201114_1721205213174_8718452800568_596.png',
                 'quantity' => 280,
             ],
             [
@@ -230,7 +230,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Appelsap',
                 'price' => 1.99,
                 'short_description' => 'Verse appelsap 1L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_appelsap.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/17092024_1726580061742_1726580070150_8718452648955_1.png',
                 'quantity' => 220,
             ],
             [
@@ -238,7 +238,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Sinaasappelsap',
                 'price' => 2.49,
                 'short_description' => 'Verse sinaasappelsap 1L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_sinaasappelsap.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/12122024_1734021201864_1734021213353_8718452864331_596.png',
                 'quantity' => 210,
             ],
             [
@@ -246,7 +246,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Paprika Chips',
                 'price' => 1.29,
                 'short_description' => 'Heerlijke paprika chips 200g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_paprika_chips.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/10012025_1736526807134_1736526812871_8710398532244_1.png',
                 'quantity' => 400,
             ],
             [
@@ -254,15 +254,15 @@ class ProductSeeder extends Seeder
                 'name' => 'Naturel Chips',
                 'price' => 1.29,
                 'short_description' => 'Heerlijke naturel chips 200g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_naturel_chips.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/13012025_1736767986445_1736767991140_8710398532268_1.png',
                 'quantity' => 380,
             ],
             [
                 'sub_category_id' => $chocoladeSubCategory->id,
-                'name' => 'Melkchocolade',
+                'name' => 'Chocolade Melk',
                 'price' => 1.99,
                 'short_description' => 'Heerlijke melkchocolade 100g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_melkchocolade.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/24122024_1735006348869_1735006356351_75729_PAK_08712800188322_H1N1.png',
                 'quantity' => 500,
             ],
             [
@@ -270,7 +270,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Pure Chocolade',
                 'price' => 2.49,
                 'short_description' => 'Heerlijke pure chocolade 100g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_pure_chocolade.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/15012025_1736906636209_1736906657027_73780_PAK_07622400013423_C1N1_s01.png',
                 'quantity' => 450,
             ],
             [
@@ -278,7 +278,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Margherita Pizza',
                 'price' => 3.99,
                 'short_description' => 'Heerlijke Margherita pizza',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_margherita_pizza.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/06052024_1714998812790_1714998822423_4001724604501_1.png',
                 'quantity' => 120,
             ],
             [
@@ -286,7 +286,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Salami Pizza',
                 'price' => 4.49,
                 'short_description' => 'Heerlijke Salami pizza',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_salami_pizza.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/26042024_1714127650071_1714127684048_8718452777020_596.png',
                 'quantity' => 110,
             ],
             [
@@ -294,7 +294,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Vanille IJs',
                 'price' => 2.99,
                 'short_description' => 'Heerlijk vanille ijs 500ml',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_vanille_ijs.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/26062024_1719412466335_1719412472977_8718452774548_1.png',
                 'quantity' => 90,
             ],
             [
@@ -302,7 +302,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Chocolade IJs',
                 'price' => 3.49,
                 'short_description' => 'Heerlijk chocolade ijs 500ml',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_chocolade_ijs.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/12042024_1712946979301_1712946987806_8718452773664_1.png',
                 'quantity' => 80,
             ],
             [
@@ -310,7 +310,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Verse Basilicum',
                 'price' => 1.99,
                 'short_description' => 'Verse basilicum 30g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_verse_basilicum.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/11102024_1728660847285_1728660853781_8718452841981_1.png',
                 'quantity' => 300,
             ],
             [
@@ -318,7 +318,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Verse Peterselie',
                 'price' => 1.99,
                 'short_description' => 'Verse peterselie 30g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_verse_peterselie.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/29072024_1722270917081_1722270923850_8718452796403_1.png',
                 'quantity' => 320,
             ],
             [
@@ -326,7 +326,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Allesreiniger',
                 'price' => 2.99,
                 'short_description' => 'Allesreiniger 1L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_allesreiniger.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/29092023_1695996695829_1695996698370_8718452727469_1.png',
                 'quantity' => 150,
             ],
             [
@@ -334,7 +334,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Glasreiniger',
                 'price' => 2.49,
                 'short_description' => 'Glasreiniger 1L',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_glasreiniger.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/29092023_1695996695795_1695996699357_8718452727445_1.png',
                 'quantity' => 140,
             ],
             [
@@ -342,7 +342,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Toiletpapier',
                 'price' => 3.99,
                 'short_description' => 'Toiletpapier 6 rollen',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_toiletpapier.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/09082023_1691593426401_1691593434792_8718452707584_1.png',
                 'quantity' => 200,
             ],
             [
@@ -350,7 +350,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Keukenpapier',
                 'price' => 2.99,
                 'short_description' => 'Keukenpapier 4 rollen',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_keukenpapier.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/21112023_1700580790012_1700580801377_8718452789474_1.png',
                 'quantity' => 180,
             ],
             [
@@ -358,7 +358,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Babyvoeding',
                 'price' => 4.99,
                 'short_description' => 'Babyvoeding 400g',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_babyvoeding.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/24072024_1721835319962_1721835325291_8445290844170_1.png',
                 'quantity' => 100,
             ],
             [
@@ -366,7 +366,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Luiers',
                 'price' => 8.99,
                 'short_description' => 'Luiers maat 4, 50 stuks',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_luiers.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/20022025_1740021736618_1740021738040_561257_STK_08700216366755_C1N1.png',
                 'quantity' => 80,
             ],
             [
@@ -374,7 +374,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Hondenvoer',
                 'price' => 5.99,
                 'short_description' => 'Hondenvoer 2kg',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_hondenvoer.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/25042024_1714055710143_1714055715112_8718452532216_1.png',
                 'quantity' => 150,
             ],
             [
@@ -382,7 +382,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Kattenvoer',
                 'price' => 4.99,
                 'short_description' => 'Kattenvoer 2kg',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_kattenvoer.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/02042024_1712068508793_1712068535830_8718452746439_596.png',
                 'quantity' => 140,
             ],
             [
@@ -390,7 +390,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Multivitamines',
                 'price' => 9.99,
                 'short_description' => 'Multivitamines 60 tabletten',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_multivitamines.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/22022025_1740186093943_1740186100079_8713713023588_1.png',
                 'quantity' => 200,
             ],
             [
@@ -398,7 +398,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Shampoo',
                 'price' => 3.99,
                 'short_description' => 'Shampoo 500ml',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_shampoo.png',
+                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/28092023_1695890048436_1695890051564_8720181327698_1.png',
                 'quantity' => 180,
             ],
             [
@@ -406,7 +406,7 @@ class ProductSeeder extends Seeder
                 'name' => 'Douchegel',
                 'price' => 2.99,
                 'short_description' => 'Douchegel 500ml',
-                'img_url' => 'https://www.jumbo.com/dam-images/fit-in/360x360/Products/1234567890_douchegel.png',
+                'img_url' => 'http://jumbo.com/dam-images/fit-in/360x360/Products/22012025_1737545567748_1737545573039_8710447284094_1.png',
                 'quantity' => 150,
             ],
         ]);
